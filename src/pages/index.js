76846493d481b009b28b317696a763b43f263eda
Sergey Kulchytski <serg@ib.by>
2022-01-16 13:08:52 +0300

    add spinner

--- src/pages/index.js
+++ src/pages/index.js
@@ -83,7 +83,9 @@ const IndexPage = ({data}) => {
       })}
     </div>
     <div ref={loadRef}>
-      {hasMore ? <p>Loading...</p> : ''}
+      {hasMore ? <div className="spinner-wrap"><svg className="spinner" viewBox="0 0 50 50">
+          <circle className="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+        </svg></div> : ''}
     </div>
   </Layout>
 )}
