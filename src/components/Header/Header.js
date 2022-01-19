28015416cd19d56fde615e122b1b2fea4d30bdec
Sergey Kulchytski <serg@ib.by>
2022-01-19 22:58:26 +0300

    add logo, favicon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,14 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import logo from "../../images/logo.png"
 
 const Header = ({ siteTitle }) => {
 
   return (
     <header className='header'>
       <div className='container'>
-        <>
-          Logo
-          <nav>
-            <Link to="/" >{siteTitle}</Link>
-          </nav>
-        </>
+        <Link to="/" className="logo" ><img src={ logo } alt={siteTitle} /></Link>
       </div>
     </header>
   )}
